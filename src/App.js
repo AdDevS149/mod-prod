2c5190a255e26202308ecd0f64a517dcab73fd50
amartin58 <adee1001@outlook.com>
2022-05-04 16:24:23 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header >
+    <a href="/">LUX BAGS</a>
+      </header>
+      <main>
+List producrs
+         
+      </main>
+    </div>
+  );
+}
+
+export default App;
